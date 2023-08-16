3a47998e6459c206eb5ce0dcc0d73ed206134b50
israa <ISRAAQW@ac.sce.ac.il>
2023-08-16 22:54:06 +0300

    israa

---
+++ constants/data.js
@@ -0,0 +1,139 @@
+import { images, icons } from '../constants'
+
+export const donors = [
+    {
+        id: '1',
+        image: images.user1,
+        name: 'Yasin Hossain',
+        location: 'Bear Sheva',
+        bloodType: 'A+',
+    },
+    {
+        id: '2',
+        image: images.user2,
+        name: 'Mohammed Sami',
+        location: 'Tel Aviv',
+        bloodType: 'AB+',
+    },
+    {
+        id: '3',
+        image: images.user3,
+        name: 'Rahimun Islam',
+        location: 'Chittagong',
+        bloodType: 'B-',
+    },
+    {
+        id: '4',
+        image: images.user4,
+        name: 'Rumana',
+        location: 'Jerusalem',
+        bloodType: 'O+',
+    },
+    {
+        id: '5',
+        image: images.user5,
+        name: 'Ali Ahmed',
+        location: 'Haifa',
+        bloodType: 'A+',
+    },
+    {
+        id: '6',
+        image: images.user6,
+        name: 'Omar Saed',
+        location: 'Ashkelon',
+        bloodType: 'B+',
+    },
+]
+
+export const categories = [
+    {
+        icon: icons.categoryIcon1,
+        title: 'Find Donors',
+    },
+    {
+        icon: icons.categoryIcon2,
+        title: 'Donates',
+    },
+    {
+        icon: icons.categoryIcon3,
+        title: 'Order Blood',
+    },
+    {
+        icon: icons.categoryIcon4,
+        title: 'Assistant',
+    },
+    {
+        icon: icons.categoryIcon5,
+        title: 'Report',
+    },
+    {
+        icon: icons.categoryIcon6,
+        title: 'Campain',
+    },
+]
+
+export const donationRequests = [
+    {
+        name: 'Israa Qweder',
+        location: 'Soroka Medical Center',
+        postedDate: '5 min',
+    },
+    {
+        name: 'Israa Qweder',
+        location: 'Soroka Medical Center',
+        postedDate: '5 min',
+    },
+    {
+        name: 'Israa Qweder',
+        location: 'Hadassah University Hospital',
+        postedDate: '5 min',
+    },
+    {
+        name: 'Israa Qweder',
+        location: 'Soroka Medical Center',
+        postedDate: '5 min',
+    },
+    {
+        name: 'Israa Qweder',
+        location: 'Soroka Medical Center',
+        postedDate: '5 min',
+    },
+    {
+        name: 'Israa Qweder',
+        location: 'Soroka Medical Center',
+        postedDate: '5 min',
+    },
+]
+
+export const features = [
+    {
+        id: '1',
+        substance: 'Glucose',
+        volume: '6 mmol/L',
+    },
+    {
+        id: '2',
+        substance: 'Cholesterol',
+        volume: '6.2 mmol/L',
+    },
+    {
+        id: '3',
+        substance: 'Bilirubin',
+        volume: '12 mmol/L',
+    },
+    {
+        id: '4',
+        substance: 'RBC',
+        volume: '3.6 ml/L',
+    },
+    {
+        id: '5',
+        substance: 'MCV',
+        volume: '102 fl',
+    },
+    {
+        id: '6',
+        substance: '276 bL',
+        volume: 'Platelets',
+    },
+]
