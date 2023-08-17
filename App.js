19fd867bc23bf739c4caa8e19ae3dc8aa79be3f0
israa <ISRAAQW@ac.sce.ac.il>
2023-08-17 19:59:42 +0300

    main

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ export default function App() {
                     name="BottomTabNavigation"
                     component={BottomTabNavigation}
                     options={{
-                        headerShown: false,
+                        headerShown: true,
                     }}
                 />
                 <Stack.Screen
